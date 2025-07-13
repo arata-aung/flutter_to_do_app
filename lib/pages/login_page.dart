1f07d6409ee47c668a9ac8e595561a74bb585c37
arata-aung <aungmyokhaing.arata@gmail.com>
2025-07-13 23:47:50 +0700

    initial - todo basic done, working login

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 50,
+              ),
+              const Icon(
+                Icons.lock,
+                size: 100,
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Welcome back',
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 20,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
